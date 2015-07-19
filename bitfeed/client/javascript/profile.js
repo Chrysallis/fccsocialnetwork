01c8c9eeba631cc8be224787f76082910fd1ea8e
Lily <lily@mail.com>
2015-07-19 11:38:28 -0600

    Merge remote-tracking branch 'upstream/master'
    Conflicts:
    	bitfeed/client/javascript/news.js
    	bitfeed/client/template/template.html

---
+++ bitfeed/client/javascript/profile.js
@@ -0,0 +1,9 @@
+/**
+ * Created by lily on 7/19/15.
+ */
+Template.buttons.events({
+    'click #profile': function() {
+        $(".profile.modal").modal('show');
+        console.log("clicky");
+    }
+});
